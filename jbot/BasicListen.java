d505e87d311ab7ea5a5a500a331b843af647b935
James W <jmontgomeryward@gmail.com>
2013-08-17 16:37:08 -0700

    Cleaned The code up, added returns and such, improved efficiency, thx reb.

--- jbot/BasicListen.java
+++ jbot/BasicListen.java
@@ -1,97 +1,106 @@
 package jbot;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+
 import org.pircbotx.hooks.Listener;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.ActionEvent;
 import org.pircbotx.hooks.events.MessageEvent;
-import org.pircbotx.PircBotX;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Random;
 public class BasicListen extends ListenerAdapter implements Listener {
 
-	
-   
+
+
 	//@Override
 	//public void onJoin(JoinEvent event) {
-		//if (event.getUser().getNick().toString() != "epic_jbot"){
-		// event.getBot().sendMessage(event.getChannel(), "Welcome, " +  event.getUser().getNick().toString());
+	//if (event.getUser().getNick().toString() != "epic_jbot"){
+	// event.getBot().sendMessage(event.getChannel(), "Welcome, " +  event.getUser().getNick().toString());
 
-	
-		//}
-	
+
 	//}
-	
-	
-	
-	
+
+	//}
+
+
+
+
+	@Override
+	public void onAction(ActionEvent event) throws Exception{
+		String firedmessage = null;
+		if (event.getMessage().contains("vlees")) {
+			firedmessage = event.getUser().getNick() + ", you're fired.";
+			event.getBot().sendMessage("#p_slice", firedmessage);
+		}
+
+
+	}
 	@Override
 	public void onMessage(MessageEvent event) throws Exception {
-		
+
+		String message = event.getMessage();
+
 		try {
-		if (event.getMessage().startsWith("vlee"))
-			 event.getBot().sendMessage("#p_slice", "Aww.");
-		
-		if (event.getMessage().contains("spam"))
-		 event.getBot().sendMessage("#p_slice", "Spam, spam, spam, spam, SPASPAAM!");
-		
-		
-		
-		if (event.getMessage().contains("epic_jbot")){
-		
-		
-		if (event.getMessage().startsWith("epic_jbot, pls")){
-			 event.getBot().sendMessage("#p_slice", "It's what I do!");
-		} else if (event.getMessage().startsWith("epic_jbot pls")){
-			 event.getBot().sendMessage("#p_slice", "You got a problem with me!?!");
-		} else if ((event.getMessage().equalsIgnoreCase(("epic_jbot, I KILL YOU")) && (event.getUser().getNick().toString().equals("epic_jdog")))){
-              event.getBot().disconnect();
-		
-		} else {
-		  
-			final Random rand = new Random();
+			if (message.startsWith("vlee"))
+				event.getBot().sendMessage("#p_slice", "Aww.");
 
-			String[] phrases = {"Yo!", "That would be this fine bot right here.", "This is the jbot you're looking for!", "Yea Buddy!", "jbot is best bot!"};
-			String response = phrases[rand.nextInt(phrases.length)];
-			event.getBot().sendMessage("#p_slice", response);
-			
-		
+			if (message.contains("spam"))
+				event.getBot().sendMessage("#p_slice", "Spam, spam, spam, spam, SPASPAAM!");
+
+
+
+			if (message.contains("epic_jbot")){
+				if (message.contains("epic_jbot")){
+
+
+					if (message.startsWith("epic_jbot, pls")){
+						event.getBot().sendMessage("#p_slice", "It's what I do!");
+						return;
+					}
+					if (message.startsWith("epic_jbot pls")){
+						event.getBot().sendMessage("#p_slice", "You got a problem with me!?!");
+						return;
+					}
+
+					if (message.equals("epic_jbot, I KILL YOU") && event.getUser().getNick().toString().equals("epic_jdog")){
+						event.getBot().disconnect();
+						return;
+					}
+
+
+					final Random rand = new Random();
+
+					String[] phrases = {"Yo!", "That would be this fine bot right here.", "This is the jbot you're looking for!", "Yea Buddy!", "jbot is best bot!"};
+					String response = phrases[rand.nextInt(phrases.length)];
+					event.getBot().sendMessage("#p_slice", response);
+
+
+				}
+
+
+			}
+			if (message.startsWith("!time")){
+
+				Calendar calendar = Calendar.getInstance();;
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+				String currenttime = null;
+
+				currenttime = sdf.format(calendar.getTime());
+
+				event.getBot().sendMessage("#p_slice", "It's currently " + currenttime + " PST.");
+			}
+
 		}
-		  
-		  
-		}
-		  if (event.getMessage().startsWith("!time")){
-				 
-			  Calendar calendar = Calendar.getInstance();;
-		    	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-		    	 String currenttime = null;	  
-			  
-			  currenttime = sdf.format(calendar.getTime());
-			  
-	    		event.getBot().sendMessage("#p_slice", "It's currently " + currenttime + " PST.");
-		  }
-		
-		} 
 		catch (Exception ey) {
-		ey.printStackTrace();
-		System.out.println("IT DONE GOOFT ON LISTENING TO CHAT!!");
-		
+			ey.printStackTrace();
+			System.out.println("IT DONE GOOFT ON LISTENING TO CHAT!!");
+
+		}
 	}
-	}
-	@Override	  
-	public void onAction(ActionEvent event) throws Exception{
-		 String firedmessage = null;
-			  if (event.getMessage().contains("vlees")) {
-				firedmessage = event.getUser().getNick() + ", you're fired.";
-			  	event.getBot().sendMessage("#p_slice", firedmessage);
-			  }
-			 
-			  
-		  }
 
 
 
 
-	}
+}
 
 
